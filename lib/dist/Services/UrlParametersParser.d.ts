535b29266a7c9b4c612aa42c8ce5083b6772381f
Alexey Solovyov <plotinka@lyoha.info>
2020-07-09 15:43:18 +0300

    09.07 Updated files

---
+++ lib/dist/Services/UrlParametersParser.d.ts
@@ -0,0 +1,6 @@
+import { Route, RouteObject } from '../Router/types';
+export default class UrlParametersParser {
+    private static getQueryParams;
+    private static getPathParams;
+    static createRouteObject(matchedRoutes: Route[], url: string): RouteObject;
+}
