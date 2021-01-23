48b76e64e843a1af62902439f120e60e45c2ad03
Alexey Solovyov <plotinka@lyoha.info>
2021-01-23 18:57:26 +0300

    fix: missing routes because of not full search

--- lib/src/utils/parsing/parseRoutes.ts
+++ lib/src/utils/parsing/parseRoutes.ts
@@ -1,16 +1,39 @@
+function getRoutesTreeChain(
+  allRoutes: RouteMatchData[],
+  currentRoute: RouteMatchData
+) {
+  if (!currentRoute) return [];
+  const tree: RouteMatchData[] = [currentRoute];
+  let currentSeekingId: string | null = currentRoute.parentId;
+  do {
+    const seed = allRoutes.find(
+      (seedRoute) => seedRoute.id === currentSeekingId
+    );
+    seed && tree.push(seed);
+    currentSeekingId = seed?.parentId ?? null;
+  } while (currentSeekingId);
+  return tree;
+}
+
 export function parseRoutes(routes: RouteMatchData[], url: string) {
+  const allMatched: RouteMatchData[] = [];
   const usedIds: string[] = [];
   const usedDepths: number[] = [];
-
-  return routes.filter((route) => {
-    if (
-      usedIds.includes(route.id) ||
-      usedDepths.includes(route.nestingDepth) ||
-      !route.regexpPath.test(url)
-    )
-      return false;
-    usedIds.push(route.id as string);
-    usedDepths.push(route.nestingDepth as number);
-    return true;
+  routes.forEach((route) => {
+    if (route.regexpPath.test(url)) {
+      allMatched.push(...getRoutesTreeChain(routes, route));
+    }
   });
+  const match = allMatched.filter((route) => {
+    if (
+      !usedIds.includes(route.id) &&
+      !usedDepths.includes(route.nestingDepth)
+    ) {
+      usedIds.push(route.id);
+      usedDepths.push(route.nestingDepth);
+      return true;
+    }
+    return false;
+  });
+  return match;
 }
